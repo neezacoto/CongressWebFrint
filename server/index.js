d023a2b550dfbe28a8a43f77bd53d379bb0af716
neezacoto <ringthebellpepper@gmail.com>
2021-03-08 18:18:45 -0500

    adding files

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const MY_PORT = 9999;
 
 app.use('/Politician', CongressApi);
 
-app.use('/',Express.static('./client'));
+app.use('/',Express.static('../client'));
 
 app.listen(MY_PORT, ()=>{
     console.log(`Server started on port: ${MY_PORT}`);
